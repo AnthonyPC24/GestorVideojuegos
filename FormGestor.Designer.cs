0c71cf99c4b2673cbbca8c8e1f56dc59bb310d41
Anthony Paz Coral <thony1124@gmail.com>
2025-11-11 17:44:23 +0100

    11/11/2025

---
+++ FormGestor.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace GestorJuegos
+{
+    partial class FormGestor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelImagen = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelImagen
+            // 
+            this.panelImagen.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelImagen.Location = new System.Drawing.Point(0, 0);
+            this.panelImagen.Name = "panelImagen";
+            this.panelImagen.Size = new System.Drawing.Size(200, 552);
+            this.panelImagen.TabIndex = 2;
+            // 
+            // FormGestor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 552);
+            this.Controls.Add(this.panelImagen);
+            this.Name = "FormGestor";
+            this.Text = "FormGestor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelImagen;
+    }
+}
---
+++ FormInicio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+using GestorJuegos.Modelos;
+
+namespace GestorJuegos
+{
+    public partial class FormInicio : Form
+    {
+        // Creamos el gestor de usuarios
+        private GestorUsuarios gestor = new GestorUsuarios();
+
+        public FormInicio()
+        {
+            InitializeComponent();
+        }
+
+        // Botón Acceder
+        private void buttonAcceder_Click(object sender, EventArgs e)
+        {
+            string usuario = textBoxUsuario.Text.Trim();
+            string contraseña = textBoxContraseña.Text.Trim();
+
+            if (gestor.ValidarUsuario(usuario, contraseña))
+            {
+                // Si el login es correcto, abrimos el FormGestor
+                FormGestor formGestor = new FormGestor();
+                formGestor.Show();
+
+                // Ocultamos el login
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos.",
+                                "Error",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+
